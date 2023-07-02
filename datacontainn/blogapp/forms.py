e4e79846017d44a255c5b2b4329c9e426ab85af9
Derrick Moseti <mosetiderrick1@gmail.com>
2023-07-02 20:58:18 +0300

    modified login

--- datacontainn/blogapp/forms.py
+++ datacontainn/blogapp/forms.py
@@ -1,15 +1,39 @@
 from django import forms
-from .models import Post
+from .models import Post,Category
+
+choices = [('coding','coding'),('linux','linux'),('How to','How to')]
+
+cats = Category.objects.all().values_list('name','name')
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
+        fields = ('title','title_tag', 'author','category','body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control','placeholder':'This is a title place holder'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=cats,attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'})
+        }
+
+
+
+
+
+
+class EditForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
         fields = ('title','title_tag', 'author','body')
 
-        widget = {
+        widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control','placeholder':'This is a title place holder'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'})
         }
+
--- datacontainn/blogapp/models.py
+++ datacontainn/blogapp/models.py
@@ -8,12 +8,24 @@ from django.utils import timezone
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255,default='My Data Science Blog')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_created=True,default=timezone.now)
+    category  = models.CharField(max_length=255, default= 'BlogTopics')
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
